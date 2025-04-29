f462eb1491a8d83334aa21664d13b594b6047345
haruki <kinikun@outlook.com>
2025-04-29 18:42:20 +0800

    安装了scss

--- app.vue
+++ app.vue
@@ -6,8 +6,19 @@ if (a === 1) {
 </script>
 <template>
   <div>
+    <div class="test-box">
+      <div class="test">测试</div>
+    </div>
+
     <NuxtRouteAnnouncer />
 
     <NuxtWelcome />
   </div>
 </template>
+<style lang="scss" scoped>
+.test-box {
+  .test {
+    background-color: red;
+  }
+}
+</style>
