c034bb176cb143b0d2f6658a71f10fc4d744adf4
haruki <kinikun@outlook.com>
2025-05-02 19:08:31 +0800

    element-plus

--- app.vue
+++ app.vue
@@ -3,11 +3,17 @@ const a = 1
 if (a === 1) {
   console.log(a)
 }
+ElMessage('hello')
 </script>
 <template>
   <div>
     <div class="test-box">
       <div class="test">测试</div>
+      <div>
+        <el-button @click="ElMessage('hello')">button</el-button>
+        <ElButton :icon="ElIconEditPen" type="success">button</ElButton>
+        <LazyElButton type="warning">lazy button</LazyElButton>
+      </div>
     </div>
 
     <NuxtRouteAnnouncer />
